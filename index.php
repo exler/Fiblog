ab92e8dbd094d0d3effb3290f481d1d0f604921f
Kamil Marut <kakkmar@gmail.com>
2018-09-28 14:49:07 +0200

    Create site layout

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php // Fiblog - source: https://github.com/EXLER/Fiblog
+
+define("CONTENT_DIR", __DIR__ . "/content/");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta content="width=device-width, initial-scale=1.0" name="viewport">
+                <title>
+                    Webpage Title
+                </title>
+                <meta content="" name="description">
+                    <link href="/assets/main.css" rel="stylesheet">
+                        <link href="/assets/favicon.png" rel="icon">
+                        </link>
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <div class="menu">
+            <div class="title">
+                Webpage Title
+            </div>
+            <a href="/">
+                home
+            </a>
+            <ul class="social">
+                
+            <ul>
+        </div>
+        <div class="content">
+        	<h1>
+            	Articles on this webpage:
+        	</h1>
+        </div>
+        <hr>
+    </body>
+</html>
